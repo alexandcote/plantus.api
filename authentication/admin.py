e5c366930e444f1830714ba3699227c1d7c4a258
Hugo Dorion <hugo.dorion@gmail.com>
2017-02-06 13:25:11 -0500

    New User form in admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from authentication.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('__str__', 'email')
+    list_filter = ()
+    ordering = ('email',)
+    fieldsets = (
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'email', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_staff', 'is_superuser', 'groups',
+                       'user_permissions')}),
+        (_('Important dates'), {
+            'fields': ('last_login', 'created_at', 'updated_at')}),
+    )
+    readonly_fields = ('created_at', 'updated_at',)
+
+admin.site.register(User, CustomUserAdmin)
+
