f90385a1e6e7bbd36a4bdbe4bdfdfc8626e75e51
Jo Boucher <j_boucher741@hotmail.com>
2017-02-15 10:10:02 -0500

    Fix flake8

--- places/views.py
+++ places/views.py
@@ -1,5 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from places.models import Place
