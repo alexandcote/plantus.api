e9c1fb1ff1c8e41069dea2dab84ab56a577c2f65
Alexandre Côté <a.cote@2kloc.com>
2017-02-21 23:19:23 -0500

    Fix permission on timeseries

--- pots/permissions.py
+++ pots/permissions.py
@@ -12,11 +12,14 @@ class PotsPermission(permissions.BasePermission):
 
 class TimeSeriesPermission(permissions.BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated():
+            return False
+
         if view.action in ['destroy', 'update', 'partial_update', 'create']:
-            return request.user.is_authenticated() and \
-                   request.user.is_superuser
+            return request.user.is_superuser
+
         return True
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_superuser or \
-               obj.place in request.user.places.all()
+               obj.pot.place in request.user.places.all()
