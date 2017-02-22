82d70b8b809189568c2c1ade55eada9a0f6f48ca
Alexandre Côté <a.cote@2kloc.com>
2017-02-21 21:17:47 -0500

    Add Timeseries

--- pots/admin.py
+++ pots/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from pots.models import Pot
+from pots.models import (
+    Pot,
+    TimeSerie
+)
 
 
 @admin.register(Pot)
 class PotAdmin(admin.ModelAdmin):
     list_display = ('name', 'place', 'plant')
     list_filter = ('place__users',)
+
+
+@admin.register(TimeSerie)
+class TimeSeriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pot', 'date',)
+    list_filter = ('pot', 'pot__place__users',)
+    list_display_links = ('id', 'pot',)
--- pots/models.py
+++ pots/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from places.models import Place
 from plants.models import Plant
 
+from django.utils.translation import ugettext_lazy as _
+
 
 class Pot(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
@@ -16,3 +18,32 @@ class Pot(models.Model):
          Display the pot object
         """
         return self.name
+
+
+class TimeSerie(models.Model):
+
+    pot = models.ForeignKey(Pot, on_delete=models.CASCADE, blank=None,
+                            null=None, related_name='timeseries')
+    date = models.DateTimeField(auto_now_add=True)
+    temperature = models.DecimalField(
+        blank=False, null=False, verbose_name=_('Temperature'),
+        max_digits=4, decimal_places=2
+    )
+    humidity = models.DecimalField(
+        blank=False, null=False, verbose_name=_('Humidity'),
+        max_digits=4, decimal_places=2
+    )
+    luminosity = models.DecimalField(
+        blank=False, null=False, verbose_name=_('Luminosity'),
+        max_digits=4, decimal_places=2
+    )
+    water_level = models.DecimalField(
+        blank=False, null=False, verbose_name=_('Water level'),
+        max_digits=4, decimal_places=2
+    )
+
+    def __str__(self):
+        """
+         Display the time serie object
+        """
+        return "{0} - {1}".format(self.pot, self.date)
