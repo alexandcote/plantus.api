c73060f6d057f80e1f26088ffe3d9b5a02965cdc
Alexandre Côté <a.cote@2kloc.com>
2017-04-02 15:06:43 -0400

    Add task to create operation for water

--- pots/tasks.py
+++ pots/tasks.py
@@ -1,10 +1,38 @@
+from django.db.models import Avg
+
 from plantus.celery import app
+from pots.models import (
+    Pot,
+    TimeSerie,
+    Operation)
+
+NB_VALUE = 10
 
 
-@app.task(bind=True)
-def sensor_analyser():
+# @app.task(bind=True)
+def pots_analyser():
     """
-    Task that analyse each pot of the database and create operation
+    Task that analyse each pot of the database
     """
-    # TODO: Discuss how we do this...
-    print("Analyse ...")
+    # Todo Add some tests
+    pots = Pot.objects.prefetch_related('plant').all()
+    for pot in pots:
+        pot_analyser.delay(pot)
+
+
+@app.task
+def pot_analyser(pot):
+    """
+    Task that analyse a pot and create operation
+    """
+    # Todo Add some tests
+    time_series_average = TimeSerie.objects\
+        .filter(pot=pot)\
+        .values('pot_id')\
+        .order_by('-date')[:NB_VALUE]\
+        .aggregate(average_humidity=Avg('humidity'))
+
+    if pot.plant.humidity_spec > time_series_average['average_humidity'] \
+            and not Operation.objects.filter(
+            pot=pot, action_id='water', completed_at__isnull=True).exists():
+        Operation(action_id='water', pot=pot).save()
